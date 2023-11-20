1471d2a039d4503ff5854d864cb1722ba374ada2
Kavin <140937467+KavinK5@users.noreply.github.com>
2023-11-20 12:30:33 +0530

    functions

---
+++ Tute2.js
@@ -0,0 +1,54 @@
+function redmi12(){
+    return 'Returned REDMI';
+}
+
+console.log(redmi12());
+
+function redmi(){
+    return 'Returned REDMI';
+} 
+
+console.log(redmi);
+
+// With Return
+
+function redmi21(){
+    return ('Returned REDMI');
+}
+
+console.log(typeof redmi21);
+
+function redmi55(){
+    return ('Returned REDMI');
+}
+
+console.log(typeof redmi55());
+
+
+// Console log
+
+function redmi77(){
+    console.log('Returned REDMI5');
+}
+
+console.log(typeof redmi77);
+
+function redmiNote7(){
+    console.log('Returned REDMI77');
+}
+
+console.log(typeof redmiNote7() + 'here');
+
+
+
+function redmiNote7Pro(pr6){
+    console.log(pr6 + pr6);
+}
+
+redmiNote7Pro();
+
+function redmiNote8(pr6){
+    console.log(pr6);
+}
+
+redmiNote8();
